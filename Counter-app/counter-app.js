52cf112fa5f492b40af0099594b536b8a5e187d8
ismailhiz <isomaohiz123@gmail.com>
2024-07-18 01:34:35 +0300

    ı make counter

---
+++ Counter-app/counter-app.js
@@ -0,0 +1,56 @@
+//  how to up for new counter
+
+
+// ----------------------------------------------------------------------------------
+
+// let sayac = 0;
+
+// function artan(){
+//     sayac++;
+//    let yenisayac = document.getElementById('sayaclar').innerHTML=sayac;
+
+// }
+
+// function azalan(){
+//     sayac--;
+//     document.getElementById('sayaclar').innerHTML =sayac;
+// }
+
+
+
+// ----------------------------------------------------------------------------------
+// doubling counter
+
+// let sayac = 0;
+
+// function ikikatartan(){
+//     sayac++;
+//     let ikikatsayac = sayac+sayac;
+//    document.getElementById('sayaclar').innerHTML=ikikatsayac;
+
+// }
+
+// function ikikatazalan(){
+//     sayac--;
+//     let ikikatazalansayac = sayac + sayac;
+//     document.getElementById('sayaclar').innerHTML =ikikatazalansayac;
+// }
+
+
+// ----------------------------------------------------------------------------------
+
+
+let sayac = 0;
+
+function ikikatartan(){
+    sayac++;
+    let ikikatsayac = sayac+sayac;
+   document.getElementById('sayaclar').innerHTML=ikikatsayac;
+
+}
+
+function ikikatazalan(){
+    sayac--;
+    let ikikatazalansayac = sayac + sayac;
+    document.getElementById('sayaclar').innerHTML =ikikatazalansayac;
+}
